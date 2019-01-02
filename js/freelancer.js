00560055cba271824673b881b46a39c991e47e68
stanleyzuo <sjrzuo@gmail.com>
2019-01-01 16:32:50 -0800

    replaced old site files with new site files

---
+++ js/freelancer.js
@@ -0,0 +1,101 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 70)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Change nav bar bg color and rotate toggle icon on menu expand
+  $(".nav-link, #bars").click(function(){
+    if ($(".navbar-toggler").attr("aria-expanded") == "false") {
+      $("#mainNav").addClass("bg-secondary");
+      $("#bars").addClass("fa-rotate-90 color-primary");
+    } else {
+      $("#mainNav").removeClass("bg-secondary");
+      $("#bars").removeClass("fa-rotate-90 color-primary");
+    }
+  });
+
+  //Remove bg color and icon rotate on navbar brand click
+  $(".navbar-brand").click(function() {
+    if ($(".navbar-toggler").attr("aria-expanded") == "true") {
+      $("#mainNav").removeClass("bg-secondary");
+      $("#bars").removeClass("fa-rotate-90 color-primary");
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 80
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Modal popup$(function () {
+  $('.portfolio-item').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#username',
+    modal: true
+  });
+  $(document).on('click', '.portfolio-modal-dismiss', function(e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+  });
+
+  // Floating label headings for the contact form
+  $(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+      $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+      $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+      $(this).removeClass("floating-label-form-group-with-focus");
+    });
+  });
+
+  // Toggle floating nav
+  $("#lock").click(function(){
+    $("#lock").toggleClass("fa-lock fa-lock-open");
+    $("#mainNav").toggleClass("fixed");
+  });
+
+})(jQuery); // End of use strict
+
